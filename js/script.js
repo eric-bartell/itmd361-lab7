926235a0561bb213cfefb6d1cd12c0859459c5cc
eric-bartell <31963754+eric-bartell@users.noreply.github.com>
2019-11-19 21:42:49 -0600

    added js, first test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  document.getElementById("entrybutton").addEventListener("click", buttonclick);
 
-
-
-
-
+  function buttonclick(){
+  	 var input = document.getElementById("entryinput");
+  	 alert("Eric Bartell: "+input.value);
+  	 document.getElementById("textoutput") = input.value;
+  }  
+}
 
 
 window.addEventListener('load', init);
